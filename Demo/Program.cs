79f63b0905be779f397f89adcdf716d7177139d7
Mo <Mo@DESKTOP-7P4PPCV>
2025-01-11 19:59:44 +0200

    UserDefined Casting Operator

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Drawing;
 using System.Numerics;
 using Demo.Abstraction;
+using Demo.Mapping;
 using Demo.operatorOverloading;
 
 namespace Demo
@@ -39,17 +40,85 @@ namespace Demo
             // 4 + 3 i
             // 4 + 3 ت
 
+            //MyComplex C01 = new MyComplex()
+            //{
+            //    Real = 3,
+            //    Imag = 4
+            //};
+            //MyComplex C02 = new MyComplex()
+            //{
+            //    Real = 5,
+            //    Imag = 6
+            //};
+            //MyComplex C03 = C01 + null;
+            //Console.WriteLine(C03);
+
+            //Console.WriteLine(++C01);
+
+            //if (C01 > C02)
+            //{
+            //    Console.WriteLine("C01 is greater than C02");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("C01 is not greater than C02");
+
+            //}
+            #endregion
+
+            #region UserDefined Casting Operator
+
+            // double X=12;
+
+            //int Y=(int)X;
+
+            // (int) : Casting Operator
+
             MyComplex C01 = new MyComplex()
             {
-                Real=3,Imag=4
+                Real = 3,
+                Imag = 4
             };
-            MyComplex C02 = new MyComplex()
+            string S = (string)C01;
+            // (string) : Convert Complex Number To String
+
+            /// Operator Overloading
+            /// user-Defiend Casting Operator
+            /// 
+
+            //Console.WriteLine(S);
+            //int X = (int)C01;
+            //Console.WriteLine(X);
+
+            int X =(int) new MyComplex() { Real = 10, Imag = 20 };
+
+
+            // Mapping :convert from Datatype To Datatype
+
+            // code first
+            // DB First
+
+            // User [Id,Name,Email,UserName,PhoneNum,CardNum,ExipreDate]
+            // UserViewModel
+            // USerDto
+            // [Name,Email,PhoneNum]
+
+            // Mannual Mapping
+
+            User user = new User();
+            UserDto userDto = new UserDto()
             {
-                Real=5,Imag=6
+                Email = user.Email,
+                Name = user.Name,
+                PhoneNum = user.PhoneNum
             };
-            MyComplex C03 = C01 + null;
-            Console.WriteLine(C03);
+
+            // Casting Operator
+
+            // Automapper : Mapping Automatic
+
             #endregion
+
         }
     }
 }

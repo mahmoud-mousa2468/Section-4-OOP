cb47ed7f717b5f60a2dcd3a0f78867a4a570adbe
Mo <Mo@DESKTOP-7P4PPCV>
2025-01-11 15:38:16 +0200

    Operator Overloading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Drawing;
+using System.Numerics;
 using Demo.Abstraction;
+using Demo.operatorOverloading;
 
 namespace Demo
 {
@@ -21,13 +23,32 @@ namespace Demo
 
             // shape.GetArea;
 
-            Rectangley rectangle = new Rectangley()
+            //Rectangley rectangle = new Rectangley()
+            //{
+            //    Dim01 = 12,
+            //    Dim02 = 3
+            //};
+            //Console.WriteLine(rectangle.Perimeter);
+            //Console.WriteLine(rectangle.GetArea());
+            #endregion
+            #region Operator Overloading
+            //int x=3+4;
+            //string y = "Hello" + "World";
+            // Complex Number : Real +Imag
+
+            // 4 + 3 i
+            // 4 + 3 ت
+
+            MyComplex C01 = new MyComplex()
             {
-                Dim01 = 12,
-                Dim02 = 3
+                Real=3,Imag=4
             };
-            Console.WriteLine(rectangle.Perimeter);
-            Console.WriteLine(rectangle.GetArea());
+            MyComplex C02 = new MyComplex()
+            {
+                Real=5,Imag=6
+            };
+            MyComplex C03 = C01 + null;
+            Console.WriteLine(C03);
             #endregion
         }
     }
---
+++ Demo/operatorOverloading/Complex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.operatorOverloading
+{
+    internal class MyComplex
+    {
+        public int Real { get; set; }
+        public int Imag { get; set; }
+
+        //Operator Overloading
+        // Must be Puplic & Static
+        // Binary Operator + - / * %
+        public static MyComplex operator +(MyComplex left, MyComplex right)
+        {
+            return new MyComplex()
+            {
+                Real=(left?.Real ?? 0)+(right?.Real ?? 0),
+                Imag=(left?.Imag ?? 0)+(right?.Imag ?? 0)
+            };
+        }
+        public static MyComplex operator -(MyComplex left, MyComplex right)
+        {
+            return new MyComplex()
+            {
+                Real = (left?.Real ?? 0) - (right?.Real ?? 0),
+                Imag = (left?.Imag ?? 0) - (right?.Imag ?? 0)
+            };
+        }
+        public override string ToString()
+        {
+            return $"{Real} + {Imag} i";
+        }
+    }
+}

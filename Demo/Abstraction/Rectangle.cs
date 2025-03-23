c12754d32d37d6b3adeac21a0d8644e35d8948d2
200~mahmoud-mousa2468 <mo.mousa2468@gmail.com>
2025-03-23 09:08:18 +0200

    new

---
+++ Demo/Abstraction/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Abstraction
+{
+    // concrete class fully Implemented
+    internal class Rectangle : Shape /* Implement And Inherit From  Shape*/
+    {
+        public override double Perimeter
+        {
+            get
+            {
+                return Dim01 * Dim02;
+            }
+        }
+        public override double GetArea()
+        {
+            return Dim01 * Dim02;
+        }
+    }
+}
---
+++ Demo/Abstraction/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Abstraction
+{
+    internal abstract class Shape
+    {
+        public double Dim01 { get; set; }
+        public double Dim02 { get; set; }
+
+        // Abstract Method : like Virtual Method Without Implementation
+        public abstract double GetArea();
+
+        public abstract double Perimeter
+        {
+            get;
+        }
+    }
+}
